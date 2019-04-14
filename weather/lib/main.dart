6aef430e682adf85d57ec09e117e4d54ee853f7f
amin.ios <amin030296@gmail.com>
2019-04-14 13:58:09 +0500

    Added
    data

--- weather/lib/main.dart
+++ weather/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:weather/pages/main_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Container(),);
+    return MaterialApp(
+      title: 'Weather',
+      home: MainPage(),
+    );
   }
 }
-
-
-
