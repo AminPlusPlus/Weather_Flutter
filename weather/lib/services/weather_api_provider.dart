beafd8c5902155964bb57bbbfb2dfd122fca35de
amin.ios <amin030296@gmail.com>
2019-04-30 10:43:17 +0500

    Models
    added

---
+++ weather/lib/services/weather_api_provider.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/api_location_models.dart';
+
+class WeatherApiProvider {
+  String baseURL = "";
+  String _keyApi = "52df6be14a24fa15796ec767c63bfc23";
+}
